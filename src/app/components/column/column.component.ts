2ffad318db7a2e74b18ee0ec7ae1df9a0199bdd6
Marc Bakker <webdev@marcbakker.com>
2023-04-22 21:35:32 +0200

    Column component toevoegen

--- src/app/components/column/column.component.ts
+++ src/app/components/column/column.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { WordleService } from 'src/app/services/wordle.service';
 
 @Component({
   selector: 'app-column',
   templateUrl: './column.component.html',
-  styleUrls: ['./column.component.scss']
+  styleUrls: ['./column.component.scss'],
 })
 export class ColumnComponent implements OnInit {
+  activeRow!: number;
 
-  constructor() { }
+  constructor(private wordleService: WordleService) {}
 
   ngOnInit(): void {
+    this.activeRow = this.wordleService.getActiveRow();
   }
-
 }
