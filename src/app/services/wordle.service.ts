d0c4503bede1db2fe0cf10b0b78979fbea7c2af3
Marc Bakker <webdev@marcbakker.com>
2023-04-23 20:50:29 +0200

    WordleService: toevoegen 5 letter woorden, checkUserGuess(), updateActiveRow().

--- src/app/services/wordle.service.ts
+++ src/app/services/wordle.service.ts
@@ -1,36 +1,85 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class WordleService {
-  guessCount = 0;
-  numberOfRows = 6;
-  numberOfCellsPerRow = 5;
   activeRow = 0;
+  guessCount = 0;
+  guessResult = '';
+  numberOfCellsPerRow = 5;
+  numberOfRows = 6;
+  wordToGuess = '';
+  words = [
+    'kraal',
+    'appel',
+    'groen',
+    'graag',
+    'greep',
+    'kramp',
+    'aarde',
+    'ander',
+    'ronde',
+  ];
 
-  constructor() {}
+  rowSubject = new BehaviorSubject<number>(0);
 
-  checkUserGuess(input: string, index: number) {
+  constructor() {
+    const randomIndex = Math.floor(Math.random() * this.words.length);
+    this.wordToGuess = this.words[randomIndex];
+  }
+
+  /**
+   * description - check input - aangeroepen vanuit de Tile component
+   * @param guessedLetter {string} - de letter die de speler intypt
+   * @param letterIndex  {number} - de positie van de letter in het te raden woord
+   * @returns {string} - 'bijnaGoed' | 'goed' | '' resp letter komt voor in woord | op juiste plaats | komt niet voor
+   */
+
+  checkUserGuess(guessedLetter: string, letterIndex: number): string {
+    let guessResult = '';
+
+    if (this.wordToGuess.includes(guessedLetter)) {
+      guessResult = 'bijnaGoed';
+      if (this.wordToGuess[letterIndex] === guessedLetter) {
+        guessResult = 'goed';
+      }
+    }
+    this.updateActiveRow();
+
+    return guessResult;
+  }
+
+  /**
+   * - description: voedt de BehaviourSubject met nieuwe waarde. Column component is subscriber
+   */
+  updateActiveRow() {
     this.guessCount++;
-    this.activeRow = this.guessCount / this.numberOfCellsPerRow;
+    this.activeRow = Math.floor(this.guessCount / this.numberOfCellsPerRow);
+    this.rowSubject.next(this.activeRow);
   }
 
-  cellIsEditable(cellIndex: number) {
-    return (
-      cellIndex >= this.activeRow * this.numberOfCellsPerRow &&
-      cellIndex < (this.activeRow + 1) * this.numberOfCellsPerRow
-    );
-  }
-
+  /**
+   *
+   * @returns {number}- aantal rijen in spel
+   */
   getNumberOfRows() {
     return this.numberOfRows;
   }
 
+  /**
+   *
+   * @returns {number}- aantal cellen per rij
+   */
   getNumberOfCellsPerRow() {
     return this.numberOfCellsPerRow;
   }
 
+  /**
+   *
+   * @returns {number}- de rij (0-N) met editable cellen
+   */
   getActiveRow() {
     return this.activeRow;
   }
